1bc3b07c8574314c02339d6fe7292b7bccad89fd
Gaston <gastonamarilla2013@gmail.com>
2020-05-13 11:32:14 -0300

    Agregando imputs y textarea

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.use((req, res , next) => {
 
 //Routes
 app.use(require('./routes/authentication'));
-app.use('.links',require('./routes/links'));
+app.use('/links',require('./routes/links'));
 app.use(require('./routes'));
 
 //Public
--- src/routes/links.js
+++ src/routes/links.js
@@ -2,16 +2,12 @@ const express = require ('express');
 const router = express.Router();
 const pool = require ('../database');
 
-
-
-
-
-
-
-
-
-
-
+router.get('/add', (req ,res )=>{
+    res.render('links/add');
+});
+router.post('/add', (req,res)=>{
+    res.send('recive');
+});
 
 module.exports = router;
 
